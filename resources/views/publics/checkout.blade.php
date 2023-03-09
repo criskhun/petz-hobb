71039cbc4b7ce47c978b555469a2bb4e52f07246
Saycon <saycon.karl@gmail.com>
2023-03-09 15:10:56 +0800

    Update checkout.blade.php

--- resources/views/publics/checkout.blade.php
+++ resources/views/publics/checkout.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app_public')
 
 @section('content')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <div class="checkout-page">
     <div class="container">
         <div class="row">
