29276d2e24237e2c6ee2d7d7f951afbb870fa78b
Saycon <saycon.karl@gmail.com>
2023-05-22 07:59:22 +0800

    update

--- app/Http/Controllers/PaypalController.php
+++ app/Http/Controllers/PaypalController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use Srmklive\PayPal\Services\ExpressCheckout;
 use Illuminate\Http\Request;
-use NunoMaduro\Collision\Provider;
+use #\Collision\Provider;
 use App\Models\Cart;
 use App\Models\Product;
 use DB;
