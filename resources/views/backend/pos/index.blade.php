e21c82f3fdda0bddb1b842b296c84d54cbbf8cd6
Saycon <saycon.karl@gmail.com>
2023-06-16 03:06:01 +0800

    Update index.blade.php

--- resources/views/backend/pos/index.blade.php
+++ resources/views/backend/pos/index.blade.php
@@ -75,6 +75,7 @@
 @endsection
 
 @push('scripts')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
     // Add product to selected list
     function addProduct(productId) {
