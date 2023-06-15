fbed8593d2c37a642007b8ceb7a7588bc734367f
Saycon <saycon.karl@gmail.com>
2023-06-16 03:15:07 +0800

    Update header.blade.php

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -102,7 +102,7 @@
                                     @endphp
                                 @endforeach
                            @endif
-                            <a href="{{route('wishlist')}}" class="single-icon"><i class="fa fa-heart-o"></i> <span class="total-count">{{Helper::wishlistCount()}}</span></a>
+                            <!-- <a href="{{route('wishlist')}}" class="single-icon"><i class="fa fa-heart-o"></i> <span class="total-count">{{Helper::wishlistCount()}}</span></a> -->
                             <!-- Shopping Item -->
                             @auth
                                 <div class="shopping-item">
