856b6ba084babffca40f48005a187ae1f70ab70a
Saycon <saycon.karl@gmail.com>
2023-06-16 00:41:47 +0800

    Update POSController.php

--- app/Http/Controllers/POSController.php
+++ app/Http/Controllers/POSController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\POS;
 use Illuminate\Http\Request;
 use App\Models\Cart;
-class CouponController extends Controller
+class POSController extends Controller
 {
     /**
      * Display a listing of the resource.
